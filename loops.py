8717d33370a38eef79479aea5e3da62b84ea885a
JoshuaImmortal <jshkomolafe89@gmail.com>
2023-05-11 17:15:36 +0100

    First Push

---
+++ loops.py
@@ -0,0 +1,31 @@
+# x = ["red", "blue", "green"]
+# profile = [1, 2, 3, 4]
+# print(profile[1])
+# for y in x:
+#     for d in profile:
+#         print(y)
+
+
+# the range function
+# for x in range(20, 10, -2):
+#     print(x)
+# the while loop
+# i=1
+# while i<=6:
+#     print(i)
+#     i += 1
+
+# x = ["red", "blue", "red", "green", "read", "yellow"]
+# length = len(x) - 3
+# print(x[length])
+
+# x = 0
+# while x<=10:
+#    print(x * 2)
+#    x += 1
+
+
+x = 0
+while x<=10:
+   x += 1
+   print(x * 2)
